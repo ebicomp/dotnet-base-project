9decda406928516723a6afe28ee74bc7c826f40b
akarimi <ebicomp@gmail.com>
2023-08-20 19:07:19 +0200

    add persistence database

--- HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
+++ HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -2,48 +2,60 @@
 using HR.LeaveManagement.Domain;
 using HR.LeaveManagement.Domain.Common;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
-namespace HR.LeaveManagement.Persistence.DatabaseContext
+namespace HR.LeaveManagement.Persistence.DatabaseContext;
+
+public class HrDatabaseContext : DbContext
 {
-    public class HrDatabaseContext : DbContext
+    public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options) : base(options)
     {
-        public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options) : base(options)
+
+    }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseDomainEntity>()
+            .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
         {
+            entry.Entity.LastModified = DateTime.Now;
 
+            if (entry.State == EntityState.Added)
+                entry.Entity.DateCreated = DateTime.Now;
         }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
+        //modelBuilder.Entity<LeaveType>()
+        //    .HasData(
+        //            new LeaveType
+        //            {
+        //                Id = 1,
+        //                Name = "Vacation",
+        //                DefaultDays = 30,
+        //                DateCreated = DateTime.Now,
+        //                LastModified = DateTime.Now
+        //            }
+        //    );
+        base.OnModelCreating(modelBuilder);
+    }
+}
 
-        public DbSet<LeaveType> LeaveTypes { get; set; }
-        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
-        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+public class HRDbContextFactory : IDesignTimeDbContextFactory<HrDatabaseContext>
+{
+    public HrDatabaseContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<HrDatabaseContext>();
+        optionsBuilder.UseSqlServer("server=localhost;database=HrManagement;User Id=sa;Password=reallyStrongPwd123;TrustServerCertificate=true;");
 
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-        {
-            foreach (var entry in base.ChangeTracker.Entries<BaseDomainEntity>()
-                .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
-            {
-                entry.Entity.LastModified = DateTime.Now;
-
-                if (entry.State == EntityState.Added)
-                    entry.Entity.DateCreated = DateTime.Now;
-            }
-            return base.SaveChangesAsync(cancellationToken);
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
-            //modelBuilder.Entity<LeaveType>()
-            //    .HasData(
-            //            new LeaveType
-            //            {
-            //                Id = 1,
-            //                Name = "Vacation",
-            //                DefaultDays = 30,
-            //                DateCreated = DateTime.Now,
-            //                LastModified = DateTime.Now
-            //            }
-            //    );
-            base.OnModelCreating(modelBuilder);
-        }
+        return new HrDatabaseContext(optionsBuilder.Options);
     }
 }
 
---
+++ HR.LeaveManagement.Persistence/Migrations/20230820165535_InitialMigration.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.LeaveManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LeaveTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DefaultDays = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveAllocations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberOfDays = table.Column<int>(type: "int", nullable: false),
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    Period = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveAllocations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveAllocations_LeaveTypes_LeaveTypeId",
+                        column: x => x.LeaveTypeId,
+                        principalTable: "LeaveTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    DateRequested = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RequestComments = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateActioned = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Approved = table.Column<bool>(type: "bit", nullable: true),
+                    Cancelled = table.Column<bool>(type: "bit", nullable: true),
+                    RequestingEmployeeId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequests_LeaveTypes_LeaveTypeId",
+                        column: x => x.LeaveTypeId,
+                        principalTable: "LeaveTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "LeaveTypes",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DefaultDays", "LastModified", "LastModifiedBy", "Name" },
+                values: new object[] { 1, null, new DateTime(2023, 8, 20, 18, 55, 35, 750, DateTimeKind.Local).AddTicks(8720), 30, new DateTime(2023, 8, 20, 18, 55, 35, 750, DateTimeKind.Local).AddTicks(8760), null, "Vacation" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveAllocations_LeaveTypeId",
+                table: "LeaveAllocations",
+                column: "LeaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequests_LeaveTypeId",
+                table: "LeaveRequests",
+                column: "LeaveTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeaveAllocations");
+
+            migrationBuilder.DropTable(
+                name: "LeaveRequests");
+
+            migrationBuilder.DropTable(
+                name: "LeaveTypes");
+        }
+    }
+}
